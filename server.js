692cc81841b6eedf418c0b27f3eb3abca6562301
tuffylock <tuffylock@gmail.com>
2016-02-15 03:25:03 -0800

    add connection and drawing listeners to server.js

--- server.js
+++ server.js
@@ -9,4 +9,23 @@ var app = express();
 var server = http.createServer(app);
 var io = socketIo.listen(server);
 server.listen(8080);
+app.use(express.static(__dirname + '/'));
 console.log("Server running on 127.0.0.1:8080");
+
+
+var line_history = [];
+
+// when new client connects
+io.on('connection', function (socket) {
+
+  // catch client up on drawings from before they arrived
+  for (var i in line_history) {
+    socket.emit('draw_line', { line: line_history[i] });
+  }
+
+  // add new lines to line_history, send to connected clients
+  socket.on('draw_line', function (data) {
+    line_history.push(data.line);
+    io.emit('draw_line', { line: data.line });
+  });
+});
